e5c1cb6c716b41dac2b58d70d4e459c01d85cfce
GivenTeacher5 <108965476+GivenTeacher5@users.noreply.github.com>
2022-07-14 10:04:40 -0500

    Create IndentSyntax.py
    All of the indenting syntax + commenting

---
+++ IndentSyntax.py
@@ -0,0 +1,35 @@
+#Introduction to indentations
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+#^ Indent example
+
+'''
+
+if 5 > 2:
+print("Five is greater than two!")
+
+Failed indent example
+
+'''
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!")
+ 
+# Number of spaces don't matter
+
+'''
+
+if 5 > 2:
+ print("Five is greater than two!") 
+
+        print("Five is greater than two!")
+
+As long as it stays the same number
+
+''' 
+
+#ps, not sure if you want an entirely different py file for comment example
